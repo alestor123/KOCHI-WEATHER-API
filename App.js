cdae3d37ac88338454b4b7e11a94f9abd4e60aa9
alestor123 <alestoraldous@gmail.com>
2020-09-09 20:56:59 +0530

    Setting key

--- App.js
+++ App.js
@@ -2,5 +2,6 @@ var express =  require('express'),
     app = express(),
     axios = require('axios');
     require('dotenv').config()
-    PORT = process.env.PORT || 3000;
+ var PORT = process.env.PORT || 3000,
+    key = process.env.KEY;
 
