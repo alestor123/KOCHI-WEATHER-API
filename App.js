daf57b882b0a7c1cacd186a0fd0e904829262d82
alestor123 <alestoraldous@gmail.com>
2020-09-10 11:35:10 +0530

    testing limiter

--- App.js
+++ App.js
@@ -8,9 +8,10 @@ key = process.env.KEY,
 city = process.env.CITY,
 limiter = rateLimit({
   windowMs: process.env.LIMTIM * 60 * 1000, 
-  max: 15 // limit each IP to 100 requests per windowMs
+  max: 3 // limit each IP to 100 requests per windowMs
 }),
 url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+app.use(limiter);
 if(!process.env){
 console.log('Please enter env')
 process.exit(1)
